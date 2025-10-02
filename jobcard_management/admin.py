703396ed4272c64228f5fdb2a0b2c26dbe241b81
glodi <111871909+glodikalombo04@users.noreply.github.com>
2025-10-03 01:16:00 +0200

    Update admin.py

--- jobcard_management/admin.py
+++ jobcard_management/admin.py
@@ -3,12 +3,12 @@ from .models import SupportAgent, JobType, JobCardCounter, Accessories, JobCard
 from simple_history.admin import SimpleHistoryAdmin
 from backend_core.models import Region, Technician, Customer
 
-
+'''
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
     search_fields = ["name"]
-
+'''
 
 @admin.register(Technician)
 class TechnicianAdmin(admin.ModelAdmin):
@@ -16,13 +16,13 @@ class TechnicianAdmin(admin.ModelAdmin):
     search_fields = ["name", "initials"]
     list_filter = ["region"]
 
-
+'''
 @admin.register(Customer)
 class CustomerAdmin(SimpleHistoryAdmin):
     list_display = ["id", "name", "region"]
     search_fields = ["name"]
     list_filter = ["region"]
-
+'''
 
 @admin.register(SupportAgent)
 class SupportAgentAdmin(admin.ModelAdmin):
